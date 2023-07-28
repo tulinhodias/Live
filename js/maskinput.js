b5a4b245236a1e426dc6e48b9ddd3f665d736167
tulinhodias <grupovitalizare@gmail.com>
2023-07-28 14:47:51 -0300

    alteração de site

---
+++ js/maskinput.js
@@ -0,0 +1,35 @@
+// Aguarde o documento ser carregado
+$(document).ready(function() {
+    // Selecione o campo de entrada pelo ID e aplique a máscara
+    $('#telefone1').mask('(00) 0000-00009');
+
+    // Adicione um evento de envio ao formulário
+    $('#formulario-contato1').submit(function(event) {
+      // Obtém o valor do campo de telefone
+      let telefone = $('#telefone').val();
+
+      // Remove os caracteres indesejados (espaços, parênteses e traço)
+      let numeros = telefone.replace(/\D/g, '');
+
+      // Atualiza o valor do campo de telefone com apenas os dígitos
+      $('#telefone').val(numeros);
+    });
+  });
+
+  // Aguarde o documento ser carregado
+$(document).ready(function() {
+    // Selecione o campo de entrada pelo ID e aplique a máscara
+    $('#telefone2').mask('(00) 0000-00009');
+
+    // Adicione um evento de envio ao formulário
+    $('#formulario-contato2').submit(function(event) {
+      // Obtém o valor do campo de telefone
+      let telefone = $('#telefone').val();
+
+      // Remove os caracteres indesejados (espaços, parênteses e traço)
+      let numeros = telefone.replace(/\D/g, '');
+
+      // Atualiza o valor do campo de telefone com apenas os dígitos
+      $('#telefone').val(numeros);
+    });
+  });
